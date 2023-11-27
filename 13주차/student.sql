a631d07a7bfc2f26a8bbb817120f9fd005ba1c11
Sam <inshsam3@gmail.com>
2023-11-28 00:32:10 +0900

    1128

---
+++ 13주차/student.sql
@@ -0,0 +1,20 @@
+CREATE TABLE student (
+    Id      varchar(5), 
+    Pass    varchar(20),
+    Address varchar(40), 
+    primary key (ID)
+);
+
+INSERT INTO student VALUES('00128','1111','서울 용산구 한강대로 405');
+INSERT INTO student VALUES('12345','2222','경기 수원시 팔달구 덕영대로 924');
+INSERT INTO student VALUES('19991','3333','강원 정선군 정선읍 녹송 8길 55');
+INSERT INTO student VALUES('23121','4444','인천광역시 중구 월미로 6');
+INSERT INTO student VALUES('44553','5555','충남 천안시 동남구 대흥로 239');
+INSERT INTO student VALUES('45678','6666','세종 조치원읍 으뜸길 215');
+INSERT INTO student VALUES('54321','7777','대전 동구 중앙로 215');
+INSERT INTO student VALUES('55739','8888','충북 청주시 정봉동 182-1');
+INSERT INTO student VALUES('70557','9999','경북 구미시 구미중앙로 76');
+INSERT INTO student VALUES('76543','1234','전북 정읍시 서부산업도로 305');
+INSERT INTO student VALUES('76653','2345','경남 김해시 진영읍 김해대로 809');
+INSERT INTO student VALUES('98765','3456','부산 동구 중앙대로 206');
+INSERT INTO student VALUES('98988','4567','제주특별자치도 제주시 용담이동 2002');
